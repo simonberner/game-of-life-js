e80a6705c86e4ee9639381a81a3407bfe71fde41
Simon Berner <sibebzh@gmail.com>
2024-02-08 20:52:05 +0100

    refactor: :recycle: refactor test #1

--- tests/GameOfLife.test.js
+++ tests/GameOfLife.test.js
@@ -1,11 +1,10 @@
 import { calcNextGen } from "../src/GameOfLife";
 
-// An empty set reflects an empty universe
-let initialUniverse = new Set();
+let emptyUniverse = new Set();
 
 // #1
-describe('calcNextGen', () => {
-    it('should return an empty universe if a empty universe is passed in.', () => {
-        expect(calcNextGen(initialUniverse)).toEqual(new Set());
+describe('calcNextGen with an empty universe', () => {
+    it('should return an empty universe.', () => {
+        expect(calcNextGen(emptyUniverse)).toEqual(new Set());
     });
 });
