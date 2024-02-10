eadd5719335c788493a8a7488bdf3b3cdd254a9c
Simon Berner <sibebzh@gmail.com>
2024-02-10 17:37:36 +0100

    test: :test_tube: add a failing test #2

--- tests/GameOfLife.test.js
+++ tests/GameOfLife.test.js
@@ -8,3 +8,12 @@ describe('calcNextGen with an empty universe', () => {
         expect(calcNextGen(emptyUniverse)).toEqual(new Set());
     });
 });
+
+// 2 dimensions coordinates=: row, column
+let universeWithABlock = new Set([1,1], [1,2], [2,1], [2,2]);
+// #2: Still Lifes - Block
+describe("calcNextGen with a Block", () => {
+    it('should return a universe with a Block.', () => {
+        expect(calcNextGen(universeWithABlock)).toEqual(universeWithABlock);
+    });
+})
