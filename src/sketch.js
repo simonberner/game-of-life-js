3dc728d8d64b9a435bc0239ae4e365d5808d2b81
Simon Berner <sibebzh@gmail.com>
2024-03-13 10:59:09 +0100

    add: :sparkles: add ES6 module import support

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,19 @@
+import {makeRandom2DArray} from "./GameOfLife.js";
+
+let columns;
+let rows;
+let initialUniverse;
+
 function setup() {
     createCanvas(440, 440);
+    columns = width
+    rows = height
+    initialUniverse = makeRandom2DArray(rows, columns)
 }
 
 function draw() {
     background(0);
 }
+
+window.setup = setup;
+window.draw = draw;
