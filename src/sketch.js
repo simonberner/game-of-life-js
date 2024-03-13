0130f794ea90564ac8db40e6e924e3b191a8e68e
Simon Berner <sibebzh@gmail.com>
2024-03-13 14:05:45 +0100

    add: :sparkles: draw the 2D grid
    This will draw the initialUniverse and then calculate the next universe. The draw func in p5.js is called continuously (currently in an infinit loop).

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,16 @@
-import {makeRandom2DArray} from "./GameOfLife.js";
+import {calcNextGen, makeRandom2DArray} from "./GameOfLife.js";
 
 let columns;
 let rows;
 let initialUniverse;
+let resolution = 10;
 
+/**
+ * Initializes the canvas and sets up the initial state of the universe.
+ *
+ * @function setup
+ * @returns {undefined}
+ */
 function setup() {
     createCanvas(440, 440);
     columns = width
@@ -11,8 +18,33 @@ function setup() {
     initialUniverse = makeRandom2DArray(rows, columns)
 }
 
+/**
+ * Clears the canvas and draws a 2D grid of living cells based on the initialUniverse array.
+ * The living cells are filled with black color (255).
+ *
+ * Right after the setup function, the draw function is called continuously until the program is stopped or noLoop() is called.
+ * The draw function is refreshed according to the frame rate (defined by the frameRate() function,
+ * 60 FPS by default).
+ *
+ * @returns {void}
+ */
 function draw() {
     background(0);
+
+    // Draw the 2D grid
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let x = j * resolution;
+            let y = i * resolution;
+            // Fill a living cell with black (255)
+            if (initialUniverse[i][j] === 1) {
+                fill(255);
+                stroke(0);
+                rect(x, y, resolution, resolution);
+            }
+        }
+    }
+    initialUniverse = calcNextGen(initialUniverse);
 }
 
 window.setup = setup;
